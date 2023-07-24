485a10b36b03afbbaf9c15b8c4ee9b9e76b010c9
Justin Green <justingreen.business@gmail.com>
2023-07-24 12:13:03 +1000

    Addition (Item)
    : Added  empty Item class

---
+++ Source/GameJam2023/Item.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item.h"
+
+// Sets default values for this component's properties
+UItem::UItem()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UItem::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UItem::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UItem::AddToInventory()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Item Picked UP"));
+}
+
